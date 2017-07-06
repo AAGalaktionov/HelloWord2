20b410979960712646941cf2033af62212a24aa3
user <user@debian.local>
2017-07-06 16:37:04 -0400

    Initial commit

---
+++ src/main/java/com/home/HelloWord2.java
@@ -0,0 +1,10 @@
+package com.home;
+
+/**
+ * Created by user on 7/6/17.
+ */
+public class HelloWord2 {
+    public static void main(String[] args) {
+        System.out.println("Hello WORD2");
+    }
+}
